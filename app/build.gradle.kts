922787dcec8fc15a1bb600efa8dee469c3c5abd1
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-04 05:01:19 -0500

    feat: creando la interfaz del usuario usando ROOM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ val room_version = "2.6.1"
 dependencies {
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
+    implementation(libs.androidx.media3.common.ktx)
+    implementation(libs.androidx.compiler)
     kapt("androidx.room:room-compiler:$room_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/huillca/lab07_datossinmvvm/MainActivity.kt
+++ app/src/main/java/com/huillca/lab07_datossinmvvm/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.huillca.lab07_datossinmvvm
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.OptIn
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.media3.common.util.Log
+import androidx.media3.common.util.UnstableApi
+import androidx.room.Room
 import com.huillca.lab07_datossinmvvm.ui.theme.Lab07DatosSinMVVMTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Lab07DatosSinMVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ScreenUser(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +48,97 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScreenUser(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var db: UserDatabase
+    var id        by remember { mutableStateOf("") }
+    var firstName by remember { mutableStateOf("") }
+    var lastName  by remember { mutableStateOf("") }
+    var dataUser  = remember { mutableStateOf("") }
+
+    db = crearDatabase(context)
+
+    val dao = db.userDao()
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Spacer(Modifier.height(50.dp))
+        TextField(
+            value = id,
+            onValueChange = { id = it },
+            label = { Text("ID (solo lectura)") },
+            readOnly = true,
+            singleLine = true
+        )
+        TextField(
+            value = firstName,
+            onValueChange = { firstName = it },
+            label = { Text("First Name: ") },
+            singleLine = true
+        )
+        TextField(
+            value = lastName,
+            onValueChange = { lastName = it },
+            label = { Text("Last Name:") },
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                val user = User(0,firstName, lastName)
+                coroutineScope.launch {
+                    AgregarUsuario(user = user, dao = dao)
+                }
+                firstName = ""
+                lastName = ""
+            }
+        ) {
+            Text("Agregar Usuario", fontSize=16.sp)
+        }
+        Button(
+            onClick = {
+                val user = User(0,firstName, lastName)
+                coroutineScope.launch {
+                    val data = getUsers( dao = dao)
+                    dataUser.value = data
+                }
+            }
+        ) {
+            Text("Listar Usuarios", fontSize=16.sp)
+        }
+        Text(text = dataUser.value, fontSize = 20.sp
+        )
+    }
+}
+@Composable
+fun crearDatabase(context: Context): UserDatabase {
+    return Room.databaseBuilder(
+        context,
+        UserDatabase::class.java,
+        "user_db"
+    ).build()
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab07DatosSinMVVMTheme {
-        Greeting("Android")
+suspend fun getUsers(dao:UserDao): String {
+    var rpta: String = ""
+    val users = dao.getAll()
+    users.forEach { user ->
+        val fila = user.firstName + " - " + user.lastName + "\n"
+        rpta += fila
+    }
+    return rpta
+}
+
+@OptIn(UnstableApi::class)
+suspend fun AgregarUsuario(user: User, dao:UserDao): Unit {
+    //LaunchedEffect(Unit) {
+    try {
+        dao.insert(user)
+    } catch (e: Exception) {
+        Log.e("User", "Error: insert: ${e.message}")
     }
 }
