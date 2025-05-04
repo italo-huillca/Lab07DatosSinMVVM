6a01c5ee48b4ebf4e7b6ecb9fe4d181e78eba222
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-04 04:45:47 -0500

    feat: añadiendo la entidad del usuario

---
+++ app/src/main/java/com/huillca/lab07_datossinmvvm/User.kt
@@ -0,0 +1,11 @@
+package com.huillca.lab07_datossinmvvm
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+)
